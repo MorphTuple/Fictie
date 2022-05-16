908625ef6ba854e8d47cd02bb1b240423716077f
MorphTuple <62528105+MorphTuple@users.noreply.github.com>
2022-05-16 12:35:02 +0700

    Base main UI

--- app/src/main/java/io/morphtuple/fictie/activities/main/MainActivity.kt
+++ app/src/main/java/io/morphtuple/fictie/activities/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package io.morphtuple.fictie
+package io.morphtuple.fictie.activities.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import io.morphtuple.fictie.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
