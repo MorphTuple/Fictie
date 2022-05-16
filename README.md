67c8212b48f8ca1ee9578250dec9516b9d88bf32
MorphTuple <62528105+MorphTuple@users.noreply.github.com>
2022-05-17 05:59:58 +0700

    Basic Search UI

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fictie
+
+## Goals
+
+- [ ] Search
+- [ ] Advanced Search
+- [ ] Library (Offline)
+- [ ] Read part tracker
+- [ ] Themes (Dark mode/light mode, or even different color)
+- [ ] Configurable font size and options
--- app/src/main/java/io/morphtuple/fictie/fragments/LibraryFragment.kt
+++ app/src/main/java/io/morphtuple/fictie/fragments/LibraryFragment.kt
@@ -6,22 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.morphtuple.fictie.R
-import io.morphtuple.fictie.databinding.FragmentSearchBinding
 
-class SearchFragment : Fragment() {
-    private lateinit var binding : FragmentSearchBinding
-
+class LibraryFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        binding = FragmentSearchBinding.inflate(inflater)
-
-        binding.advancedSearchBtn.setOnClickListener {
-            binding.advancedOptionsLayout.toggle()
-        }
-
-        return binding.root
+        return inflater.inflate(R.layout.fragment_library, container, false)
     }
 }
