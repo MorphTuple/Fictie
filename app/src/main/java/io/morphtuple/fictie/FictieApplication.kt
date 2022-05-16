32dad08ae2ef5ff93a26a7efd7468cb6c8b4f2f9
MorphTuple <62528105+MorphTuple@users.noreply.github.com>
2022-05-16 13:09:17 +0700

    Configure Hilt

---
+++ app/src/main/java/io/morphtuple/fictie/FictieApplication.kt
@@ -0,0 +1,8 @@
+package io.morphtuple.fictie
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class FictieApplication : Application() {
+}
--- app/src/main/java/io/morphtuple/fictie/activities/main/MainActivity.kt
+++ app/src/main/java/io/morphtuple/fictie/activities/main/MainActivity.kt
@@ -2,8 +2,10 @@ package io.morphtuple.fictie.activities.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 import io.morphtuple.fictie.R
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
